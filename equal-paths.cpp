6be667daa81eecfbe38631dbe7ddf7bbea75e3d0
Christine Jeong <jeongc@usc.edu>
2025-03-14 07:08:29 +0000

    Add equal-paths.cpp and equal-paths.h

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -1,5 +1,6 @@
 #ifndef RECCHECK
 //if you want to add any #includes like <iostream> you must do them here (before the next endif)
+#include <iostream>
 
 #endif
 
@@ -9,10 +10,40 @@ using namespace std;
 
 // You may add any prototypes of helper functions here
 
+// find depth of leaves
+int getLeafDepth(Node* root) {
+    // base case: if root is null, return -1
+    if(!root) {
+        return -1;
+    }
+    // if leaf node, return 0
+    if(!root->left && !root->right) {
+        return 0;
+    }
+
+    int leftDepth = getLeafDepth(root->left);
+    int rightDepth = getLeafDepth(root->right);
+
+    // when one child is missing
+    if(leftDepth == -1) {
+        return rightDepth + 1;
+    }
+    if(rightDepth == -1) {
+        return leftDepth + 1;
+    }
+    if(leftDepth != rightDepth) {
+        return -2;
+    }
+    return leftDepth + 1;
+}
+
 
 bool equalPaths(Node * root)
 {
-    // Add your code below
+    if(!root) {
+        return true;
+    }
 
+    return getLeafDepth(root) >= 0;
 }
 
